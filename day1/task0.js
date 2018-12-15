d1395a2c592b87a72bd0a0e385bc2530c220f54b
Robert Kwasnicki <rkwas005@wp.pl>
2018-12-15 09:26:47 +0100

    task0, bind()

--- day1/task0.js
+++ day1/task0.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+function hello() {
+    //console.log('hello world')
+    console.log(this)
+}
+
+this.hello()
+
+var person = {
+    name: 'Zenek',
+    say: hello,
+}
+
+console.log(person.say()) //?
+
+console.log('//------------------')
+
+function returnX () {
+    return this.x;
+    }
+
+var obj = { x: 42 }
+var newReturnX = returnX;
+
+console.log(newReturnX())
+
+var boundReturnX = newReturnX.bind(obj)
+console.log(boundReturnX())
