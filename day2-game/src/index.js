f9d8a457ece6791ecd5f8005699be1907d4e734d
Robert Kwasnicki <rkwas005@wp.pl>
2018-12-16 11:52:33 +0100

    game complete, for now

--- day2-game/src/index.js
+++ day2-game/src/index.js
@@ -1,80 +1,158 @@
 //przyjmuje się nazwy z dużych liter dla funkcji konstruujących
 var Game = (function() {
     function Game(gameContainer) {
-        this.gameContainer = gameContainer
+        this.gameContainer = gameContainer;
         this.deckOfCards = [{
             id: 0,
             front: 'A',
-            complete: true,
-            visible: false
+            complete: false,
+            visible: false,
         }, {
             id: 1,
             front: 'A',
             complete: false,
-            visible: true
-        }, {
+            visible: false,
+        },
+        {
             id: 2,
             front: 'B',
             complete: false,
-            visible: false
-        }, {
+            visible: false,
+        },
+        {
             id: 3,
             front: 'B',
             complete: false,
-            visible: false
-        }, {
+            visible: false,
+        },
+        {
             id: 4,
             front: 'C',
             complete: false,
-            visible: false
-        }, {
+            visible: false,
+        },
+        {
             id: 5,
             front: 'C',
             complete: false,
-            visible: false
-        }]
+            visible: false,
+        }
+    ];
+    }
 
+    Game.prototype.init = function() {
+        this.render();
     }
 
     Game.prototype.render = function() {
-        this.gameContainer.innerHtml = '' //czyścimy wnętrze kontenera
+        this.gameContainer.innerHTML = ''; //czyścimy wnętrze kontenera
 
         this.deckOfCards.forEach(function(card) {
             //console.log(card)
             var cardElement = document.createElement('div'); //tworzy div w dokumencie i przypisuje do zmiennej
 
-            cardElement.innerHTML = card.front //przepisuje wartosc z front do HTML
+            cardElement.innerHTML = card.front; //przepisuje wartosc z front do HTML
 
-            cardElement.classList.add('card') //dodanie klasy card do naszego div
+            cardElement.classList.add('card'); //dodanie klasy card do naszego div
 
             if (!card.visible && !card.complete) {
-                cardElement.classList.add('is-invisible')
+                cardElement.classList.add('is-invisible');
             }
 
-            if (!card.complete) {
-                cardElement.classList.add('is-complete')
+            if(card.complete) {
+                cardElement.classList.add('is-complete');
             }
 
-            this.gameContainer.appendChild(cardElement) // dodanie cardElement do naszego kontenera
+            this.gameContainer.appendChild(cardElement); //dodanie cardElement do naszego kontenera
 
-            cardElement.addEventListener('click', function(){
+            cardElement.addEventListener('click', function() {
                 //console.log(this)
-                console.log(card) //wyswietla daną karte w consoli
-            // })
+                //console.log(card) //wyswietla daną karte w consoli
+                this.flipCard(card);
+                this.checkGameState();
             }.bind(this))
 
-        }, this) //parametr dla forEach() wskazujący miejsce Game (bez tego odwoluje sie do Window)
+        }, this); //parametr dla forEach() wskazujący miejsce Game (bez tego odwoluje sie do Window)
     }
 
-    Game.prototype.init = function() {
-        this.render()
+    Game.prototype.flipCard = function(card) {
+        this.deckOfCards = this.deckOfCards.map(function(element) {
+            if (element.id === card.id) {
+                return Object.assign({}, card, {visible: true})
+            } else {
+                return element;
+            }
+
+            // return element.id === card.id ? Object.assign({}, card, {visible: true}) : element;
+        });
+
+        this.render();
+    };
+
+    Game.prototype.checkGameState = function() {
+        var visibleCards = this.deckOfCards.filter(function(card) {
+            return card.complete === false && card.visible === true;
+        });
+
+        if (visibleCards.length === 2) {
+            if (this.isEqual(visibleCards)) {
+                this.completeVisible();
+            } else {
+                this.hideVisible();
+            }
+        }
+
+        if (this.isGameComplete()) {
+            setTimeout(function() {
+                alert('win');
+            }, 1000);
+        }
+    };
+
+    Game.prototype.isEqual = function(visibleCards) {
+        return visibleCards.reduce(function(firstCard, secondCard) {
+            return firstCard.front === secondCard.front;
+        })
     }
 
-    return Game
-})()
+    Game.prototype.completeVisible = function() {
+        this.deckOfCards = this.deckOfCards.map(function(card) {
+            // if (card.visible) {
+            //     return  Object.assign({}, card, {complete: true});
+            // } else {
+            //     return card;
+            // }
+            //to samo jest ponizej ale w krotszej formie
+            return card.visible ? Object.assign({}, card, {complete: true}) : card;
+        })
 
-var newGame = new Game(document.querySelector('#game-container'))
+        this.render();
+    }
 
-newGame.init()
+    Game.prototype.hideVisible = function() {
+        this.deckOfCards = this.deckOfCards.map(function(card) {
+            return card.visible ? Object.assign({}, card, {visible: false}) : card;
+        })
 
-console.log(newGame)
+        setTimeout(function() {
+            this.render();
+        }.bind(this), 1000)
+    }
+
+    Game.prototype.isGameComplete = function() {
+        var uncompletedCard = this.deckOfCards.filter(function(card) {
+            return card.complete === false;
+        });
+
+        return uncompletedCard.length === 0;
+    }
+
+
+    return Game;
+})();
+
+var newGame = new Game(document.querySelector('#game-container'));
+
+newGame.init();
+
+// console.log(newGame);
